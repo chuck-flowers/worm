0576fda8b21dee402aa993a071c6bb43f07b605b
Timothy Flowers <t_chuck_flowers@yahoo.com>
2020-12-22 23:01:35 -0500

    Creates the Script trait

--- worm/src/lib.rs
+++ worm/src/lib.rs
@@ -1,3 +1,13 @@
 pub mod errors;
 pub mod results;
 pub mod sql;
+
+use self::errors::WormError;
+use self::results::QueryResults;
+use self::sql::SqlResult;
+
+pub trait Script {
+    type Output: SqlResult;
+
+    fn execute(self) -> Result<QueryResults<Self::Output>, WormError>;
+}
