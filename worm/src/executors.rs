b31410cd644df05e473f369ac5bd8f77828b954d
Timothy Flowers <t_chuck_flowers@yahoo.com>
2021-01-09 08:39:57 -0500

    Changes the execute sql method item result error from RawRowConversionError to RowConversionError

--- worm/src/executors.rs
+++ worm/src/executors.rs
@@ -1,11 +1,11 @@
 //! Types that execute raw SQL within a DBMS.
 
-use crate::errors::RawRowConversionError;
+use crate::errors::RowConversionError;
 use crate::errors::SqlExecutionError;
 use crate::sql::SqlRow;
 
 /// The iterator that is returned by an executor.
-pub type ResultIter = Box<dyn Iterator<Item = Result<SqlRow, RawRowConversionError>>>;
+pub type ResultIter = Box<dyn Iterator<Item = Result<SqlRow, RowConversionError>>>;
 
 /// A type which can execute SQL.
 pub trait SqlExecutor {
