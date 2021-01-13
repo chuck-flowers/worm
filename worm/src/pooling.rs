ac2fd147d80db74850a2f1773c5f8eb424fcc417
Timothy Flowers <t_chuck_flowers@yahoo.com>
2021-01-13 16:07:55 -0500

    Creates a 'ConnectionManager' for pooling
    Creates a type that is compatible with the r2d2 database connection
    pooling framework.

--- worm/src/pooling.rs
+++ worm/src/pooling.rs
@@ -2,3 +2,52 @@
 
 #![cfg(feature = "pooling")]
 
+use crate::connections::Connection;
+use crate::errors::WormError;
+use crate::executors::SqlExecutor;
+use crate::Script;
+use r2d2::ManageConnection;
+use std::marker::PhantomData;
+
+struct TestScript {}
+
+impl Script for TestScript {
+    type Output = ();
+
+    fn compile(self) -> String {
+        String::from("SELECT 1;")
+    }
+}
+
+/// A type that is used to manage pooled [Connection] instances.
+pub struct ConnectionManager<E>
+where
+    E: SqlExecutor + Send,
+{
+    connection_string: String,
+    __: PhantomData<E>,
+}
+
+unsafe impl<E> Sync for ConnectionManager<E> where E: SqlExecutor + Send {}
+
+impl<E> ManageConnection for ConnectionManager<E>
+where
+    E: SqlExecutor + Send + 'static,
+{
+    type Connection = Connection;
+
+    type Error = WormError;
+
+    fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        let executor = E::connect(&self.connection_string)?;
+        Ok(Connection::new(executor))
+    }
+
+    fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
+        conn.execute(TestScript {}).map(|_| ())
+    }
+
+    fn has_broken(&self, _: &mut Self::Connection) -> bool {
+        false
+    }
+}
